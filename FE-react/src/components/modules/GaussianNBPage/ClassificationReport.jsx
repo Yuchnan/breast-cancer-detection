1af393224f7ea7a758fc0c68c08b0fe8a4877571
Yuchnan <ramandhanuyuchnan@gmail.com>
2025-01-24 21:54:53 +0700

    delete unused file

--- FE-react/src/components/modules/GaussianNBPage/ClassificationReport.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ClassificationReport = () => {
-    return (
-        <div>ClassificationReport</div>
-    )
-}
-
-export default ClassificationReport
--- FE-react/src/components/modules/GaussianNBPage/ConfusionMatrix.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ConfusionMatrix = () => {
-    return (
-        <div>ConfusionMatrix</div>
-    )
-}
-
-export default ConfusionMatrix
